86179bda85f8de2edc0896df16c025ecc982863b
kalsbold <kalsbold@nate.com>
2017-06-21 18:18:39 +0900

    doublylinkedlist
    왠지 모르지만 3번 삭제하면 오류 생김

--- Data_Structure_Algorithm/DoubleLink.h
+++ Data_Structure_Algorithm/DoubleLink.h
@@ -3,11 +3,11 @@ template <typename T>
 class DNode {
 public:
 	T _element;
-	DNode * p_Next; //다음노드
-	DNode * p_Prev; //이전노드.
+	DNode<T> * p_Next; //다음노드
+	DNode<T> * p_Prev; //이전노드.
 
-	DNode() {};
-	DNode(T ele, DNode * next, DNode * prev) : _element(ele), p_Next(next), p_Prev(prev) {}
+	DNode() {}
+	DNode(T ele, DNode * prev, DNode * next) : _element(ele), p_Next(next), p_Prev(prev) {}
 };
 
 template <typename T>
@@ -16,29 +16,36 @@ public:
 	typedef DNode<T>*  pointer;
 	DoubleLink();
 	~DoubleLink();
-	//삽입.
+	//삽입. 위치와 값을 넘김.
 	DNode<T>* insert_Node(int index, T t);
+	//값을 넘김 // 첫머리에 삽입.
 	DNode<T>* insert_Head(T t);
+	//값을 넘김 // 꼬리에 삽입.
 	DNode<T>* insert_Last(T t);
 
-	//삭제
+	//삭제 위치 넘김.
 	DNode<T>* delete_Node(int index);
+	//첫 머리 삭제.
 	DNode<T>* delete_Head();
+	//꼬리 삭제.
 	DNode<T>* delete_Last();
+
 	//크기
 	int Size();
 	//비었는지
 	bool is_Empty();
 
 	//검색
-	T get_node(int index);
-	T get_head();
-	T get_last();
+	T get_node(int index) {
+		DNode<T>* pN = get_Node(index);
+		return pN->_element; }
+	T get_head() { return p_head->p_Next->_element; }
+	T get_last() { return p_head->p_Prev->_element; }
 
 	DNode<T>* get_Head();
 	DNode<T>* get_Last();
 private:
-	int node_count; // 꼬리 위치 태그.
+	int node_count; // 노드 숫자.
 	DNode<T> *p_head;
 
 	DNode<T>* get_Node(int index);
@@ -48,70 +55,104 @@ template <typename T>
 DoubleLink<T>::DoubleLink() :node_count(0), p_head(nullptr)
 {
 	p_head = new DNode<T>();
-	p_head->p_Next = nullptr;
-	p_head->p_Prev = nullptr;
+
+	p_head->p_Next = p_head;
+	p_head->p_Prev = p_head;
+
 }
 
 template <typename T>
 DoubleLink<T>::~DoubleLink(){
-	//첫 노드부터 순차적으로 삭제.
-	DNode<T>* pNode = p_head->p_Next;
-	while (nullptr != pNode)
-	{
-		//현재 노드를 임시 노드에 저장.
-		DNode<T>* temp = pNode;
-		pNode->p_Prev = p_head;//이전 노드를 헤드로.
-		pNode = pNode->p_Next; //다음 노드를 지금 노드로.
-		delete temp; // 임시노드 삭제.
-	}
+
 }
 
 template <typename T>
 DNode<T>* DoubleLink<T>::insert_Node(int index, T t){
+	//인덱스가 0 이면 첫머리에 삽입.
+	if (index == 0)
+		return insert_Head(t);
+
+	//해당하는 위치의 노드를 가져옴.
 	DNode<T> * preNode = get_Node(index); //인덱스의 노드를 가져옴.
-	if (preNode) {//존재하면.
-		//값,노드의 다음 노드의 이전노드 포인터와 노드의 다음노드 포인터 로 새 노드 생성.
-		DNode<T> * newNode = new DNode<T>(t, preNode->p_Next->p_Prev, preNode->p_Next);
-		preNode->p_Next = newNode;  //노드의 다음 노드포인터에 새 노드 저장.
-		preNode->p_Next->p_Prev = newNode; //노드의 다음 노드 이전노드 포인터에 새노드 저장.
-		node_count++; // 노드 숫자 증가.
-		return newNode; //새노드 반환.
-	}
-	return nullptr;
+	if (preNode == nullptr)
+		return nullptr;
+	//값,노드의 다음 노드 포인터와 노드 로 새 노드 생성.
+	DNode<T> * newNode = new DNode<T>(t, preNode->p_Prev, preNode);
+	preNode->p_Prev = newNode;  //노드의 다음 노드포인터에 새 노드 저장.
+	preNode->p_Prev->p_Next = newNode; //노드의 다음 노드 이전노드 포인터에 새노드 저장.
+	node_count++; // 노드 숫자 증가.
+	return newNode; //새노드 반환.
+
 }
 
 template <typename T>
 DNode<T>* DoubleLink<T>::insert_Head(T t){
-	return insert_Node(0,t);
+	DNode<T> * newNode = new DNode<T>(t, p_head, p_head->p_Next);
+	p_head->p_Next->p_Prev = newNode;
+	p_head->p_Next = newNode;
+	node_count++;
+	return newNode;
+	
 }
 
 template <typename T>
 DNode<T>* DoubleLink<T>::insert_Last(T t){
-	return insert_Node(node_count, t);
+	DNode<T> * newNode = new DNode<T>(t, p_head->p_Prev, p_head);
+	p_head->p_Prev->p_Next = newNode;
+	p_head->p_Prev = newNode;
+	node_count++;
+	return newNode;
 }
 
 //삭제
 template <typename T>
 DNode<T>* DoubleLink<T>::delete_Node(int index){
-	if (is_Empty())//비었으면.
+	if (index == 0) {
+		return delete_Head();
+	}
+	if (index == node_count - 1) {
+		return delete_Last();
+	}
+	if (index >= node_count)//비었으면.
 		return nullptr;
+
 	DNode<T>* ptrNode = get_Node(index); //인덱스 노드 호출.
-	DNode<T>* delNode = ptrNode->p_Next; //노드의 다음 노드를 삭제할 노드로 저장.
-	ptrNode->p_Next = delNode->p_Next; //노드의 다음 노드 포인터를 다음노드의 다음노드 포인터로.
-	delNode->p_Prev = ptrNode; //삭제 노드의 이전 노드포인터를 노드로.
+	
+	ptrNode->p_Prev->p_Next = ptrNode->p_Next; //노드의 다음 노드 포인터를 다음노드의 다음노드 포인터로.
+	ptrNode->p_Next->p_Prev = ptrNode->p_Prev; //삭제 노드의 이전 노드포인터를 노드로.
+	
+	DNode<T>* p_Temp = ptrNode->p_Prev; //노드의 이전노드를 반환할 노드로 저장.
+	
+
+	delete ptrNode; //노드 삭제
+
 	node_count--;//노드 숫자 감소.
-	delete delNode; //노드 삭제
-	return ptrNode->p_Next; //다음 노드 반환.
+	return p_Temp; //이전노드 반환.
 }
 
 template <typename T>
 DNode<T>* DoubleLink<T>::delete_Head(){
-	return delete_Node(0);
+	if (is_Empty())//비었으면.
+		return nullptr;
+	DNode<T>* p_Node = p_head->p_Next;
+	p_head->p_Next = p_Node->p_Next;
+	p_Node->p_Next->p_Prev = p_head;
+	delete p_Node;
+	node_count--;
+	return p_head->p_Next;
+
 }
 
 template <typename T>
 DNode<T>* DoubleLink<T>::delete_Last(){
-	return delete_Node(node_count);
+	if (is_Empty())//비었으면.
+		return nullptr;
+	DNode<T>* p_Node = p_head->p_Prev;
+	p_head->p_Prev = p_Node->p_Prev;
+	p_Node->p_Prev->p_Next = p_head;
+	delete p_Node;
+	node_count--;
+	return p_head->p_Next;
 }
 
 //크기
@@ -128,7 +169,7 @@ bool DoubleLink<T>::is_Empty(){
 
 template <typename T>
 DNode<T>* DoubleLink<T>::get_Head(){
-	return p_head->p_Next;
+	return p_head;
 }
 
 template <typename T>
@@ -142,10 +183,25 @@ DNode<T>* DoubleLink<T>::get_Node(int index){
 		return nullptr;
 	}
 	int temp = 0;
-	DNode<T>* preNode = p_head;
-	while (temp < index) {
-		temp++;
-		preNode = preNode->p_Next;
+	if (index <= node_count/2)
+	{
+		temp = index;
+		cout << "index : " << index << endl;
+		DNode<T>* nextNode = p_head->p_Next;
+		while (temp) {
+			nextNode = nextNode->p_Next;
+			temp--;
+		}
+		return nextNode;
 	}
-	return preNode;
+	else {
+		temp = node_count - index - 1;
+		cout << "index : " << index << endl;
+		DNode<T>* preNode = p_head->p_Prev;
+		while (temp) {
+			preNode = preNode->p_Prev;
+			temp--;
+		}
+		return preNode;
+	}
 }
--- Data_Structure_Algorithm/main.cpp
+++ Data_Structure_Algorithm/main.cpp
@@ -5,8 +5,12 @@
 using namespace std;
 
 /*
+완료 목록
+링크드 리스트, 원형 링크드 리스트 구현 완료
+더블 링크드 리스트(일단 구현 완료. 인덱스 3에서 삭제 오류 발생.)
+
 to do list
-리스트 부터 해시까지 구현.리스트(링크드 리스트, 더블 링크드 리스트), 스택, 큐, 힙, 트리, 그래프, 해시
+리스트 부터 해시까지 구현.스택, 큐, 힙, 트리, 그래프, 해시
 각종 알고리즘 문제 자료구조 이용해서 해결해보기.
 */
 
@@ -20,17 +24,46 @@ void main(){
 
 	DoubleLink<int> list;
 
-	for (int i = 0; i < 10; i++){
-		list.insert_Node(i, i);
+	for (int i = 0; i < 13; i++){
+		list.insert_Node(0, i*10);
+		cout << "index "<<i<<" element : " << list.get_node(i) << endl;
 	}
 
 	cout << "list size : " << list.Size() << endl;
 
+	for (int i = 0; i < list.Size(); i++)
+	{
+		cout << "index " << i << " element : " << list.get_node(i) << endl;
+	}
+
 	list.insert_Head(100);
 	list.insert_Last(200);
+	list.insert_Node(4,50);
+	list.insert_Node(4, 40);
+	list.insert_Node(4, 30);
+	list.insert_Node(4, 20);
+	list.insert_Node(4, 10);
+
+	cout << "list size : " << list.Size() << endl;
+	for (int i = 0; i < list.Size(); i++)
+	{
+		cout << "index " << i << " element : " << list.get_node(i) << endl;
+	}
+
+	cout << "list size : " << list.Size() << endl;
+	list.delete_Head();
+	list.delete_Last();
+	list.delete_Node(3);
+
+	for (int i = 0; i < list.Size(); i++)
+	{
+		cout << "index "<<i<<" element : " << list.get_node(i) << endl;
+	}
+
 
 	DoubleLink<int>::pointer ptr = list.get_Head();
-	while (ptr != nullptr)
+	ptr = ptr->p_Next;
+	while (ptr != list.get_Head())
 	{
 		cout << ptr->_element << endl;
 		ptr = ptr->p_Next;
